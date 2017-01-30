f6669534fc10f50ae9890e15875e4209c6d7b48d
Vignesh <vibala@outlook.com>
2017-01-30 12:06:49 +0100

    AfterNotification Page created temporarily

--- app/src/main/java/com/start_up/dev/apilinkus/AfterNotificationActivity.java
+++ app/src/main/java/com/start_up/dev/apilinkus/AfterNotificationActivity.java
@@ -1,10 +1,25 @@
 package com.start_up.dev.apilinkus;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.start_up.dev.apilinkus.API.APILinkUS;
 import com.start_up.dev.apilinkus.Adapter.MomentsAdapater;
@@ -26,16 +41,44 @@ public class AfterNotificationActivity extends AppCompatActivity implements Recy
     private ArrayList<Moment> moments = new ArrayList<>();
     private String TAG = AlbumFragment.class.getSimpleName();
     AlbumFragment.OnMomentSelectedListener mCallback;
-    private String userId;
     private APILinkUS api;
     private MomentsAdapater adapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_recyclerview_postnotif);
-        adapter = new MomentsAdapater(this,moments,this);
+
+        /*Planting the toolbar*/
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        View view = getLayoutInflater().inflate(R.layout.custom_action_bar_v2, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER);
+
+        TextView toolbarTitle = (TextView) view.findViewById(R.id.toolbar_title);
+        toolbarTitle.setText("Moments");
+
+        getSupportActionBar().setCustomView(view, params);
+        getSupportActionBar().setDisplayShowCustomEnabled(true); //show custom title
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //hide the default title
+
+
+        /**/
+        api = new APILinkUS();
+
+        /*Planting the recyclerView*/
         recyclerView = (RecyclerView)findViewById(R.id.moment_recyclerView_post_notif);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,1);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(dpToPx(10),1));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new MomentsAdapater(this,moments,this);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -44,7 +87,45 @@ public class AfterNotificationActivity extends AppCompatActivity implements Recy
     }
 
     @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
+
+    @Override
     public void recyclerViewListClicked(View v, int position) {
 
     }
+
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration{
+
+        private int spacing;
+        private int spanCount;
+
+        public GridSpacingItemDecoration(int spacing,int spanCount){
+            this.spacing = spacing;
+            this.spanCount = spanCount;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            outRect.left = spacing;
+            outRect.right = spacing;
+            if (position < spanCount) { // top edge
+                outRect.top = spacing;
+            }
+
+            outRect.bottom = spacing;
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
 }
